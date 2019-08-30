00133acac6e6a7229a321331464839dbad16e5a8
Jung, Kyo-Yoon <jky1234@gmail.com>
2019-08-30 16:40:16 +0900

    190830_python_basic_1
    190830_python_basic_1

---
+++ class_01_test1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 13:43:56 2019
+
+@author: jung-kyoyoon
+"""
+
+#%%
+a = 1
+b = 3
+bread = '크림빵'
+print(b)
+
+#%%
+print(bread)
+
+#%%
+print('this cell!')
---
+++ class_01_test4.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:43:15 2019
+
+@author: jung-kyoyoon
+"""
+
+#%%
+
+# '안녕'을 출력해봅시다. 
+print('안녕')
+
+#%%
+# '안녕' + '하세요' 
+print('안녕\n'+'하세요')
+
+#%%
+# '안녕','하세요'
+print('안녕','하세요')
+
+#%%
+# '1+4'를 출력해보세요. 
+print('1+4')
+
+#%%
+# 1 / 4
+print(1/4)
+
+#%%
+# 1 + 4
+print(1+4)
+
+#%%
+# '5' / 3
+print(int('5')/3)
+
+#%%
+print('5와 4의 합은 %d %s 입니다.' % (5+4,'babo'))
+
+#%%
+a = '5'
+print(a)
+b = '김치' 
+milk = '물' 
+_car = 153 
+
+a = 33
+print(a)
+
+#%%
+name = 'jane'
+age = 19
+
+print('나의 이름은 %s 이고, %d 살 입니다' % (name,age))
+
+#%%
+
+#class, if, while, def 
+
+
